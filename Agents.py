ac017714d53382eb8b8132e079684d4868c1f609
linyiii <chenlinyi1996@yahoo.com>
2017-09-13 11:07:39 -0400

    change name of agent.py to Agents.py

---
+++ Agents.py
@@ -0,0 +1,8 @@
+from game import Agent
+from game import Directions
+
+class DumbAgent(Agent):
+    "An agent that goes West until it can't"
+
+    def getAction(self,state):
+        return Directions.WEST
