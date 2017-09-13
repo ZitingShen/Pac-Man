b81b0925a21fa3b97dc43e2a5cb113896c21c3d5
linyiii <chenlinyi1996@yahoo.com>
2017-09-13 12:18:35 -0400

    add dumb agent

--- Agent.py
+++
@@ -1,8 +0,0 @@
-from game import Agent
-from game import Directions
-
-class DumbAgent(Agent):
-    "An agent that goes West until it can't"
-
-    def getAction(self.state):
-        return Directions.WEST
--- Agents.py
+++ Agents.py
@@ -4,5 +4,14 @@ from game import Directions
 class DumbAgent(Agent):
     "An agent that goes West until it can't"
 
-    def getAction(self,state):
-        return Directions.WEST
+    def getAction(self, state):
+        "The agent receives a GameState (defined in pacman.py)"
+        print 'Location:', state.getPacmanPosition()
+        print 'Actions available:', state.getLegalPacmanActions()
+        if Directions.WEST in state.getLegalPacmanActions():
+            print 'Going West'
+            return Directions.WEST
+        else:
+            print "STOP."
+            return Directions.STOP
+        
